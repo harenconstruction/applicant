c358b6299f6ef9b2c854c0dca711a5efa435010d
Andy Duncan <andy@duncaningram.com>
2016-09-29 13:14:59 -0400

    Reference child records from primary contact entity in django admin.

--- applicant/jobs/admin.py
+++ applicant/jobs/admin.py
@@ -5,9 +5,41 @@ from django.utils.safestring import mark_safe
 
 from jobs.models import Contact, EmploymentStatus, WorkExperience, Education, AdditionalInformation, Reference
 
-admin.site.register(Contact)
 admin.site.register(EmploymentStatus)
 admin.site.register(WorkExperience)
 admin.site.register(Education)
 admin.site.register(AdditionalInformation)
 admin.site.register(Reference)
+
+
+class EmploymentStatusInline(admin.TabularInline):
+    model = EmploymentStatus
+
+
+class WorkExperienceInline(admin.TabularInline):
+    model = WorkExperience
+
+
+class EducationInline(admin.TabularInline):
+    model = Education
+
+
+class AdditionalInformationInline(admin.TabularInline):
+    model = AdditionalInformation
+
+
+class ReferenceInline(admin.TabularInline):
+    model = Reference
+
+
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [
+        EmploymentStatusInline,
+        WorkExperienceInline,
+        EducationInline,
+        AdditionalInformationInline,
+        ReferenceInline,
+    ]
+
+
+admin.site.register(Contact, ContactAdmin)
