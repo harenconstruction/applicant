4666c51b174d516609571651c5c565d65f25e8f5
Andy Duncan <andy@duncaningram.com>
2016-11-15 19:04:17 -0500

    More ... production ... settings. :)

--- applicant/config/settings.py
+++ applicant/config/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.office365.com'
 EMAIL_HOST_USER = 'jobs@harenconstruction.com'
 
 #Must generate specific password for your app in [gmail settings][1]
-EMAIL_HOST_PASSWORD = 'HarenC123'
+EMAIL_HOST_PASSWORD = 'Wadu4797' # 'HarenC123'
 
 EMAIL_PORT = 587
 
