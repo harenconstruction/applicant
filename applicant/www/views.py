e9aa3a7fecd306a3eeab8ef254d19d93265d3c93
Andy Duncan <andy@duncaningram.com>
2018-10-02 11:33:06 -0400

    Hook in the slide show, or attempt this.

--- applicant/www/views.py
+++ applicant/www/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect, render, render_to_resp
 from django.template import RequestContext, TemplateDoesNotExist
 from django.template.response import TemplateResponse
 
-from pm.models import ProjectCategory, Project
+from pm.models import ProjectCategory, Project, ProjectPhoto, Slideshow
 
 
 def index(request):
@@ -33,6 +33,7 @@ def project(request, id):
     context = RequestContext(request)
     context_dict = {}
     context_dict['project'] = Project.objects.get(id=id) or None
+    context_dict['project_photos'] = Project.objects.get(id=id).photo.all()
     context_dict['project_categories'] = ProjectCategory.objects.all()
     return render_to_response('pages/projects/project.html', context_dict, context)
 
