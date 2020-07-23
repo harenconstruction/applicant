bbb6659ab0edd831a09b09b4f1e3fdcd592d0a9b
Andy Duncan <ajduncan@lakesite.net>
2020-07-23 15:39:48 -0400

    Ensure new configuration.

--- applicant/settings/components/emails.py
+++ applicant/settings/components/emails.py
@@ -2,21 +2,19 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ## old configuration:
-##EMAIL_USE_TLS = False
-##EMAIL_HOST = 'harenconstruction-com.mail.protection.outlook.com' # 'smtp.office365.com'
-##EMAIL_HOST_USER = 'jobs@harenconstruction.com'
-###Must generate specific password for your app in [gmail settings][1]
-##EMAIL_HOST_PASSWORD = 'Wadu4797' # 'HarenC123'
-##EMAIL_PORT = 25 #587
-
-# new configuration:
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_USE_TLS = False
+EMAIL_HOST = 'harenconstruction-com.mail.protection.outlook.com' # 'smtp.office365.com'
 EMAIL_HOST_USER = 'jobs@harenconstruction.com'
-EMAIL_HOST_PASSWORD = 'Son26800'
-EMAIL_PORT = 587
+#Must generate specific password for your app in [gmail settings][1]
+EMAIL_HOST_PASSWORD = 'Son26800' # 'Wadu4797' # 'HarenC123'
+EMAIL_PORT = 25 #587
 
+# noah configuration:
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.office365.com'
+#EMAIL_HOST_USER = 'jobs@harenconstruction.com'
+#EMAIL_HOST_PASSWORD = 'Son26800'
+#EMAIL_PORT = 587
 
 #This did the trick
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
