3b0db2f5e84a3d97c1f4fcd787f172a429a26c7a
Andy Duncan <ajduncan@lakesite.net>
2021-05-12 13:58:26 -0400

    Try TLS.

--- applicant/settings/components/emails.py
+++ applicant/settings/components/emails.py
@@ -2,12 +2,19 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ## old configuration:
-EMAIL_USE_TLS = False
-EMAIL_HOST = 'harenconstruction-com.mail.protection.outlook.com' # 'smtp.office365.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_HOST_USER = 'jobs@harenconstruction.com'
-#Must generate specific password for your app in [gmail settings][1]
-EMAIL_HOST_PASSWORD = 'Mut60913' # 'Son26800' # 'Wadu4797' # 'HarenC123'
-EMAIL_PORT = 25 #587
+EMAIL_HOST_PASSWORD = 'Mut60913'
+EMAIL_PORT = 587
+
+## old configuration:
+#EMAIL_USE_TLS = False
+#EMAIL_HOST = 'harenconstruction-com.mail.protection.outlook.com' # 'smtp.office365.com'
+#EMAIL_HOST_USER = 'jobs@harenconstruction.com'
+##Must generate specific password for your app in [gmail settings][1]
+#EMAIL_HOST_PASSWORD = 'Mut60913' # 'Son26800' # 'Wadu4797' # 'HarenC123'
+#EMAIL_PORT = 25 #587
 
 # noah configuration:
 #EMAIL_USE_TLS = True
