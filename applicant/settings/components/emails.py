971fc3138982306bb109fa43ecef942066dc311f
Andy Duncan <ajduncan@lakesite.net>
2021-07-12 13:55:27 -0400

    Resolves #30.

--- applicant/settings/components/emails.py
+++ applicant/settings/components/emails.py
@@ -5,7 +5,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_HOST_USER = 'jobs@harenconstruction.com'
-EMAIL_HOST_PASSWORD = 'Mut60913'
+EMAIL_HOST_PASSWORD = 'UVf7jsj02RIdR1'
 EMAIL_PORT = 587
 
 ## old configuration:
